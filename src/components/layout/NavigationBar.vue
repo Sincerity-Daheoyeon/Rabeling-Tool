869008607b71e82e9fc0d18504e25fedaa6d784d
Daeuning <happyjde3@gmail.com>
2024-10-09 19:45:52 +0900

    feat: 테스트용 sidebar 적용

---
+++ src/components/layout/NavigationBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navigation-bar">
+    <ul>
+      <li><router-link to="/">Dashboard</router-link></li>
+      <li><router-link to="/test-management">Test Management</router-link></li>
+      <li><router-link to="/user-management">User Management</router-link></li>
+      <li>
+        <router-link to="/survey-management">Survey Management</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.navigation-bar {
+  width: 250px;
+  height: 100vh;
+  background-color: #1e1e1e;
+  color: white;
+}
+
+ul {
+  list-style: none;
+  padding: 20px;
+}
+
+li {
+  margin-bottom: 15px;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
+a.router-link-active {
+  font-weight: bold;
+}
+</style>
---
+++ src/layouts/AdminDafaultLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="default-layout">
+    <NavigationBar />
+    <div class="content">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import NavigationBar from "../components/layout/NavigationBar.vue"; 
+
+export default defineComponent({
+  components: {
+    NavigationBar,
+  },
+});
+</script>
+
+<style scoped>
+.default-layout {
+  display: flex;
+}
+
+.content {
+  width: calc(100% - 250px);
+  background-color: #121212;
+  min-height: 100vh;
+  color: white;
+}
+</style>
