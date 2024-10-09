646292ddcf28b59bf3b4a29bdc1b226f220a49dc
Daeuning <happyjde3@gmail.com>
2024-10-09 19:58:01 +0900

    feat: 색상 변수 설정

--- src/layouts/AdminDafaultLayout.vue
+++ src/layouts/AdminDafaultLayout.vue
@@ -9,7 +9,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import NavigationBar from "../components/layout/NavigationBar.vue"; 
+import NavigationBar from "../components/layout/AdminSideBar.vue"; 
 
 export default defineComponent({
   components: {
